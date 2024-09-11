4cd11b744b9a1d85bc114eb2c63cae887930d326
Gustavo Carvalho <gcbrito@hotmail.com>
2024-09-11 17:57:36 -0300

    Menu da aplicação

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const start = () => {
-    let count = 1
-    while(count < 10){
-        console.log(count)
-        count = count + 1
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+
+    while(true){
+
+        const opcao = await select({ // vai aguardar a ação do usuário
+            message: "Menu >", // vai exibir essa mensagem
+            choices: [  // vai mostrar as opções abaixo
+                {
+                    name: "cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        
+        switch(opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+        }
     }
 }
 
