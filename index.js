1ed2a09e69c9790ec470522be8f9244d5b009125
Gustavo Carvalho <gcbrito@hotmail.com>
2024-09-14 13:08:52 -0300

    adiciono metas abertas

--- index.js
+++ index.js
@@ -59,8 +59,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((metas) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0){
+        console.log("não existem metas abertas! :)")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -84,6 +100,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -98,12 +118,15 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
-                console.log("vamos listar")
                 break
             case "realizadas":
-                await metasRealizadas ()
+                await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
+                console.log("Até a próxima!")
                 return
         }
     }
